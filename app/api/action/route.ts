77d640781e2d6224a229b4d94bea8b99fafb418d
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-04-06 13:23:16 -0500

    Update route.ts

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -1,6 +1,6 @@
 "use server";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://creoqsi-one.vercel.app";
 
 export async function getProducts(id?: string) {
   const url = id ? `${API_URL}/api/products?id=${id}` : `${API_URL}/api/products`;
