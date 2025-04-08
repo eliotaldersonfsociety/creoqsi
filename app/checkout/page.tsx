f85046c735b235bacca4fcd7f5b0cddc3543d4d7
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-04-08 16:45:41 -0500

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -418,3 +418,11 @@ function Lock() {
       />
       <path
         d="M7 11V7C7 5.93913 7.42143 4.92172 8.17157 4.17157C8.92143 3.42143 9.93913 3 11 3H13C14.0609 3 15.0783 3.42143 15.8284 4.17157C16.5786 4.92172 17 5.93913 17 7V11"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
