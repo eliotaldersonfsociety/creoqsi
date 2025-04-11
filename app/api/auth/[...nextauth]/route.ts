f718fd05b6a3fe36f16927a9082c22680e093410
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-04-10 21:39:08 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import db from "@/lib/db";
-import type { NextAuthConfig } from "next-auth";
+import type NextAuthConfig from "next-auth";
 
 const authOptions: NextAuthConfig = {
   providers: [
